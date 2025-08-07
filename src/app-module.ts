9dae84f823bd4662a179f18ab170be8b3dbfd10c
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2025-08-07 11:11:31 -0300

    feat: implement cqrs architecture

---
+++ src/app-module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { ApiModule } from '@api/api-module';
+import { ApplicationModule } from '@application/application-module';
+import { DomainModule } from '@domain/domain-module';
+import { InfrastructureModule } from '@infrastructure/infrastructure-module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    ApiModule,
+    ApplicationModule,
+    DomainModule,
+    InfrastructureModule,
+  ],
+})
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
+import { AppModule } from '@/app-module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(process.env.PORT ?? 3000).then(() => {
+    console.log(`🗲 - Running on PORT: ${process.env.PORT}`);
+  });
 }
-bootstrap();
+void bootstrap();
